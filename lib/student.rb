197a679c251b63df61a7d91eb8c409c016f61086
MartinCrane <martin.crane@gmail.com>
2017-02-10 10:23:21 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+  # attr_accessor :first_name, :last_name
+
+
+
+
+    def initialize
+      @knowledge = []
+      super
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(stuff)
+      self.knowledge << stuff
+    end
 
 end
